dd2a981855c9f979dd43e79759b339340bb9b4c4
aamna <atauseef41@gmail.com>
2019-12-01 04:42:20 +0500

    Vendor post work done

---
+++ Backend/university/src/main/java/com/erp/university/Purchase/Repository/VendorRepository.java
@@ -0,0 +1,9 @@
+package com.erp.university.Purchase.Repository;
+
+import com.erp.university.Purchase.Model.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
+}
