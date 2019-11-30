3486cf398fdca57c22cdb930eb9bb17e65c16a23
aamna <atauseef41@gmail.com>
2019-12-01 01:04:01 +0500

    minor head post work done

---
+++ Backend/university/src/main/java/com/erp/university/Purchase/Repository/MinorHeadRepository.java
@@ -0,0 +1,7 @@
+package com.erp.university.Purchase.Repository;
+
+import com.erp.university.Purchase.Model.MinorHead;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MinorHeadRepository extends JpaRepository<MinorHead,Long> {
+}
