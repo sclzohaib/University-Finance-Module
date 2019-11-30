d78efb8dbd85c2a68d2e13729efd9c21bffe8963
aamna <atauseef41@gmail.com>
2019-12-01 01:25:20 +0500

    covering letter post work done

---
+++ Backend/university/src/main/java/com/erp/university/Purchase/Model/CoveringLetter.java
@@ -0,0 +1,20 @@
+package com.erp.university.Purchase.Model;
+
+public class CoveringLetter {
+    private Long id;
+
+    public CoveringLetter() {
+    }
+
+    public CoveringLetter(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ Backend/university/src/main/java/com/erp/university/Purchase/Repository/CoveringLetterRepository.java
@@ -0,0 +1,7 @@
+package com.erp.university.Purchase.Repository;
+
+import com.erp.university.Purchase.Model.CoveringLetter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoveringLetterRepository extends JpaRepository<CoveringLetter,Long> {
+}
