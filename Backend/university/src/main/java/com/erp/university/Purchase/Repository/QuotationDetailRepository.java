9e15a88376a5afe04af363b68bfaa5293ad4ba73
Sunduskanwal <sunduskanwal432@gmail.com>
2019-11-30 09:40:48 -0800

    quotationDetail work done.. sundus

---
+++ Backend/university/src/main/java/com/erp/university/Purchase/Repository/QuotationDetailRepository.java
@@ -0,0 +1,11 @@
+package com.erp.university.Purchase.Repository;
+
+import com.erp.university.Purchase.Model.QuotationDetail;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuotationDetailRepository extends JpaRepository<QuotationDetail, Long> {
+
+}
