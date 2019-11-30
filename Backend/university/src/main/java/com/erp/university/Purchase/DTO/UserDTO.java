83e0167df53c2949879addf6b33ce592ec4707db
aamna <atauseef41@gmail.com>
2019-11-30 21:15:58 +0500

    user post work done

---
+++ Backend/university/src/main/java/com/erp/university/Purchase/DTO/UserDTO.java
@@ -0,0 +1,21 @@
+package com.erp.university.Purchase.DTO;
+
+public class UserDTO {
+    private Long id;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
+
---
+++ Backend/university/src/main/java/com/erp/university/Purchase/Model/User.java
@@ -0,0 +1,20 @@
+package com.erp.university.Purchase.Model;
+
+public class User {
+    private Long id;
+
+    public User() {
+    }
+
+    public User(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
