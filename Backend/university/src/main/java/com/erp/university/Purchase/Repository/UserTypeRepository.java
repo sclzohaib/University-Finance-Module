2fb08b511ff2541b1343d08b039f3a3da888bfda
aamna <atauseef41@gmail.com>
2019-11-30 20:01:23 +0500

    UserType post work done

---
+++ Backend/university/src/main/java/com/erp/university/Purchase/Repository/UserTypeRepository.java
@@ -0,0 +1,6 @@
+package com.erp.university.Purchase.Repository;
+
+import com.erp.university.Purchase.Model.UserType;
+
+public interface UserTypeRepository extends JpaRepository<UserType,Long> {
+}
