a19676cc44ae3814902648399a21880b04b22d37
Mohammad Zohaib <sclzohaib@gmail.com>
2019-12-01 20:48:48 +0500

    User refactor done

--- Backend/university/src/main/java/com/erp/university/Purchase/DTO/UserDTO.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/DTO/UserDTO.java
@@ -2,12 +2,24 @@ package com.erp.university.Purchase.DTO;
 
 public class UserDTO {
     private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String status;
+    private String address;
+    private Long contactNo;
 
     public UserDTO() {
     }
 
-    public UserDTO(Long id) {
+    public UserDTO(Long id, String name, String email, String password, String status, String address, Long contactNo) {
         this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.status = status;
+        this.address = address;
+        this.contactNo = contactNo;
     }
 
     public Long getId() {
@@ -16,6 +28,54 @@ public class UserDTO {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(Long contactNo) {
+        this.contactNo = contactNo;
     }
 }
 
--- Backend/university/src/main/java/com/erp/university/Purchase/Model/User.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/Model/User.java
@@ -1,13 +1,30 @@
 package com.erp.university.Purchase.Model;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_sequence")
+    @SequenceGenerator(name = "id_sequence", sequenceName = "user_sequence")
     private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String status;
+    private String address;
+    private Long contactNo;
 
     public User() {
     }
 
-    public User(Long id) {
-        this.id = id;
+    public User(String name, String email, String password, String status, String address, Long contactNo) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.status = status;
+        this.address = address;
+        this.contactNo = contactNo;
     }
 
     public Long getId() {
@@ -16,5 +33,53 @@ public class User {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getContactNo() {
+        return contactNo;
+    }
+
+    public void setContactNo(Long contactNo) {
+        this.contactNo = contactNo;
     }
 }
