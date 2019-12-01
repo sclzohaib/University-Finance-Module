4adea67c269789ab42bc590539637f48aa1c282d
Mohammad Zohaib <sclzohaib@gmail.com>
2019-12-01 20:58:27 +0500

    User Type refactor done

--- Backend/university/src/main/java/com/erp/university/Purchase/Service/UserTypeService.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/Service/UserTypeService.java
@@ -8,15 +8,38 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserTypeService {
     @Autowired
     UserTypeRepository userTypeRepository;
+
+    //Save
     public ResponseEntity<String> saveUserType(UserTypeDTO userTypeDTO) {
         UserType userType = new UserType();
         userType.setUserType(userTypeDTO.getUserType());
         userTypeRepository.save(userType);
         return new ResponseEntity<String>("Added Successfully", HttpStatus.CREATED);
 
+    }
+
+    //Get All
+    public ResponseEntity<List<UserType>> getAll(){
+        List<UserType> userTypes = userTypeRepository.findAll();
+        return new ResponseEntity<List<UserType>>(userTypes, HttpStatus.FOUND);
+    }
+
+    //Get By id
+    public ResponseEntity<UserType> getById(Long id){
+        UserType userType = userTypeRepository.findById(id).get();
+        return new ResponseEntity<UserType>(userType, HttpStatus.FOUND);
+    }
+
+    //Update
+    public ResponseEntity<String> update(Long id, UserTypeDTO userTypeDTO){
+        UserType userType = userTypeRepository.findById(id).get();
+        userType.setUserType(userTypeDTO.getUserType());
+        return new ResponseEntity<String>("Updated Successfully", HttpStatus.OK);
     }
 }
