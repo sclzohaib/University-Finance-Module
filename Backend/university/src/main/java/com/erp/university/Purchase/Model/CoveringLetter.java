fcec22e7c670786c4cba48ce8292a50af29f991a
Mohammad Zohaib <sclzohaib@gmail.com>
2019-12-01 06:58:51 +0500

    Covering Letter refactor done

--- Backend/university/src/main/java/com/erp/university/Purchase/Model/CoveringLetter.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/Model/CoveringLetter.java
@@ -1,6 +1,12 @@
 package com.erp.university.Purchase.Model;
 
+import javax.persistence.*;
+
+@Entity
 public class CoveringLetter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_sequence")
+    @SequenceGenerator(name = "id_sequence", sequenceName = "cl_seq")
     private Long id;
 
     public CoveringLetter() {
--- Backend/university/src/main/java/com/erp/university/Purchase/Repository/CoveringLetterRepository.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/Repository/CoveringLetterRepository.java
@@ -2,6 +2,8 @@ package com.erp.university.Purchase.Repository;
 
 import com.erp.university.Purchase.Model.CoveringLetter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CoveringLetterRepository extends JpaRepository<CoveringLetter,Long> {
 }
