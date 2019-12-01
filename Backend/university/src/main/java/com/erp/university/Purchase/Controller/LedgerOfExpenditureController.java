1754991773f6b5d037f3a3c0c0606fb3452a743a
Mohammad Zohaib <sclzohaib@gmail.com>
2019-12-01 07:39:01 +0500

    Ledger of Expenditure refactor done

--- Backend/university/src/main/java/com/erp/university/Purchase/Controller/LedgerOfExpenditureController.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/Controller/LedgerOfExpenditureController.java
@@ -1,24 +1,44 @@
 package com.erp.university.Purchase.Controller;
 
 import com.erp.university.Purchase.DTO.LedgerOfExpenditureDTO;
+import com.erp.university.Purchase.Model.LedgerOfExpenditure;
 import com.erp.university.Purchase.Service.LedgerOfExpenditureService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping  (value= "/api/ledgerOfExpenditue")
 public class LedgerOfExpenditureController {
 @Autowired
     LedgerOfExpenditureService ledgerOfExpenditureService;
+
+//POST
 @RequestMapping(value = "/",method = RequestMethod.POST)
     public ResponseEntity<String> SaveLedgerOfExpenditure(@RequestBody LedgerOfExpenditureDTO ledgerOfExpenditureDTO)
 {
     return ledgerOfExpenditureService.saveLedgerOfExpenditure(ledgerOfExpenditureDTO);
 }
-}
+
+//Get All
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity<List<LedgerOfExpenditure>> getAll(){
+    return ledgerOfExpenditureService.getAll();
+    }
+
+    //Get By id
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<LedgerOfExpenditure> getById(@PathVariable("id") Long id){
+    return ledgerOfExpenditureService.getById(id);
+    }
+
+    //Update
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<String> update(@PathVariable("id") Long id, @RequestBody LedgerOfExpenditureDTO ledgerOfExpenditureDTO){
+    return ledgerOfExpenditureService.update(id, ledgerOfExpenditureDTO);
+    }
+ }
 
--- Backend/university/src/main/java/com/erp/university/Purchase/Service/LedgerOfExpenditureService.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/Service/LedgerOfExpenditureService.java
@@ -8,11 +8,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LedgerOfExpenditureService {
 
     @Autowired
     LedgerOfExpenditureRepository ledgerOfExpenditureRepository;
+
+    //Save
     public ResponseEntity<String> saveLedgerOfExpenditure(LedgerOfExpenditureDTO ledgerOfExpenditureDTO)
     {
         LedgerOfExpenditure ledgerOfExpenditure=new LedgerOfExpenditure();
@@ -22,5 +26,28 @@ public class LedgerOfExpenditureService {
         ledgerOfExpenditure.setParticulars(ledgerOfExpenditureDTO.getParticulars());
         ledgerOfExpenditureRepository.save(ledgerOfExpenditure);
         return new ResponseEntity<String>("Added Succesfully", HttpStatus.CREATED);
+    }
+
+    //Get All
+    public ResponseEntity<List<LedgerOfExpenditure>> getAll(){
+        List<LedgerOfExpenditure> ledgerOfExpenditures = ledgerOfExpenditureRepository.findAll();
+        return new ResponseEntity<List<LedgerOfExpenditure>>(ledgerOfExpenditures, HttpStatus.FOUND);
+    }
+
+    //Get By id
+    public ResponseEntity<LedgerOfExpenditure> getById(Long id){
+        LedgerOfExpenditure ledgerOfExpenditure = ledgerOfExpenditureRepository.findById(id).get();
+        return new ResponseEntity<LedgerOfExpenditure>(ledgerOfExpenditure,HttpStatus.FOUND);
+    }
+
+    //Update
+    public ResponseEntity<String> update(Long id, LedgerOfExpenditureDTO ledgerOfExpenditureDTO){
+        LedgerOfExpenditure ledgerOfExpenditure = ledgerOfExpenditureRepository.findById(id).get();
+        ledgerOfExpenditure.setBalance(ledgerOfExpenditureDTO.getBalance());
+        ledgerOfExpenditure.setDate(ledgerOfExpenditureDTO.getDate());
+        ledgerOfExpenditure.setDebitAmount(ledgerOfExpenditureDTO.getDebitAmount());
+        ledgerOfExpenditure.setParticulars(ledgerOfExpenditureDTO.getParticulars());
+        ledgerOfExpenditureRepository.save(ledgerOfExpenditure);
+        return new ResponseEntity<>("Updated Successfully", HttpStatus.OK);
     }
 }
