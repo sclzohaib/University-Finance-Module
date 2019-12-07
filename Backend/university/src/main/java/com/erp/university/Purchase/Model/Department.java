e6b1854ac16b34ead699f36fc8ab30ab192c1844
dell <sunduskanwal4>
2019-12-07 04:29:40 -0800

    GRN JSR Validation work done ..sundus

--- Backend/university/src/main/java/com/erp/university/Purchase/Model/Department.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/Model/Department.java
@@ -10,33 +10,37 @@ public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_sequence")
     @SequenceGenerator(name = "id_sequence", sequenceName = "dept_seq")
+
     @NotNull(message = "Department ID cannot be null")
     @NotEmpty(message = "Department ID cannot be empty")
     @Column(name = "id" ,unique = true, nullable = false)
     private Long id;
 
     @NotNull(message = "Department name cannot be null")
+    @NotBlank(message = "Department name cannot be blank")
     @NotEmpty(message = "Department name cannot be null")
     @Size(min = 4 ,max = 30 , message = "Departmnet name must be in between 4 and 30")
     @Column(name = "name", unique = true ,nullable = false)
     private String name;
 
     @NotNull(message = "Department location cannot be null")
+    @NotBlank(message = "Department location cannot be blank")
     @NotEmpty(message = "Department location cannot be empty")
-    @Column(name = "location" ,unique = true, nullable = false)
+    @Column(name = "location" ,unique = false, nullable = false)
     @Size(min = 10 ,max = 100 ,message = "Department location must be in between 10 and 100")
     private String location;
 
     @NotNull(message = "Department telephone No. cannot be null")
+    @NotBlank(message = "Department telephone No. cannot be blank")
     @NotEmpty(message = "Department telephone No. cannot be empty")
     @Column(name = "telephone" ,unique = true, nullable = false)
     @Size(min = 11 ,max = 13 ,message = "Department telephone No. must be in between 11 and 13")
     private Long telephone;
 
 
-    @NotEmpty(message = "Department extension no. cannot be empty")
-    @Min ( value  =2 ,message = "Department extention no. cannot be less than 2")
-    @Max(value = 5 ,message = "Department extension no. cannot be greater than 5")
+    @NotBlank(message = "Department extension No. cannot be blank")
+    @Min ( value  =2 ,message = "Department extention No. cannot be less than 2")
+    @Max(value = 5 ,message = "Department extension No. cannot be greater than 5")
     @Column (name = "extension_no" ,unique = false ,nullable = true)
     private Long extensionNo;
 
