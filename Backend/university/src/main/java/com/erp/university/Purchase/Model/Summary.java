1a4ef6f5f844daaae0a308b9897be4024987d7f8
Muhammad mohsin <muhammadmohsinkhan864@gmail.com>
2019-12-07 21:41:50 +0500

    Summary jsr validation (sadia)

--- Backend/university/src/main/java/com/erp/university/Purchase/Model/Summary.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/Model/Summary.java
@@ -1,12 +1,18 @@
 package com.erp.university.Purchase.Model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
+@Table(name="Summary")
 public class Summary {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_sequence")
     @SequenceGenerator(name = "id_sequence", sequenceName = "summary_seq")
+    @NotNull(message = "Summary ID cannot be null")
+    @NotEmpty(message = "Summary ID cannot be empty")
+    @Column(name = "id", unique = true, nullable = false)
     private Long id;
 
     public Summary() {
