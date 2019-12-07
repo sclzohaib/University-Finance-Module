b17d60dbb06121149a3e147ffa31eb0fa9beceae
dell <sunduskanwal4>
2019-12-07 02:55:28 -0800

    Department JSR Validation work done ..sundus

--- Backend/university/src/main/java/com/erp/university/Purchase/Controller/DepartmentController.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/Controller/DepartmentController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import java.util.List;
 
 @RestController
@@ -18,7 +20,7 @@ public class DepartmentController {
 
     //Post Call
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public ResponseEntity<String> saveDepartment(@RequestBody DepartmentDTO departmentDTO){
+    public ResponseEntity<String> saveDepartment(@Valid @RequestBody DepartmentDTO departmentDTO){
         return departmentService.saveDepartment(departmentDTO);
     }
 
@@ -30,13 +32,13 @@ public class DepartmentController {
 
     //Get call to get single department by id
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Department> getDepartmentById(@PathVariable("id") Long id){
+    public ResponseEntity<Department> getDepartmentById(@PathVariable("id") @Min(value = 1,message = "ID must be greater than 1") Long id){
         return departmentService.getDepartmentById(id);
     }
 
     //Update Department via Id
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<String> updateDepartment(@PathVariable("id") Long id, @RequestBody DepartmentDTO departmentDTO){
+    public ResponseEntity<String> updateDepartment(@PathVariable("id") @Min(value = 1, message = "ID must be greater than 1") Long id, @RequestBody DepartmentDTO departmentDTO){
         return departmentService.updateDepartment(id, departmentDTO);
     }
 
--- Backend/university/src/main/java/com/erp/university/Purchase/Model/Department.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/Model/Department.java
@@ -1,17 +1,43 @@
 package com.erp.university.Purchase.Model;
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.util.List;
 
 @Entity
+@Table (name = "department")
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_sequence")
     @SequenceGenerator(name = "id_sequence", sequenceName = "dept_seq")
+    @NotNull(message = "Department ID cannot be null")
+    @NotEmpty(message = "Department ID cannot be empty")
+    @Column(name = "id" ,unique = true, nullable = false)
     private Long id;
+
+    @NotNull(message = "Department name cannot be null")
+    @NotEmpty(message = "Department name cannot be null")
+    @Size(min = 4 ,max = 30 , message = "Departmnet name must be in between 4 and 30")
+    @Column(name = "name", unique = true ,nullable = false)
     private String name;
+
+    @NotNull(message = "Department location cannot be null")
+    @NotEmpty(message = "Department location cannot be empty")
+    @Column(name = "location" ,unique = true, nullable = false)
+    @Size(min = 10 ,max = 100 ,message = "Department location must be in between 10 and 100")
     private String location;
+
+    @NotNull(message = "Department telephone No. cannot be null")
+    @NotEmpty(message = "Department telephone No. cannot be empty")
+    @Column(name = "telephone" ,unique = true, nullable = false)
+    @Size(min = 11 ,max = 13 ,message = "Department telephone No. must be in between 11 and 13")
     private Long telephone;
+
+
+    @NotEmpty(message = "Department extension no. cannot be empty")
+    @Min ( value  =2 ,message = "Department extention no. cannot be less than 2")
+    @Max(value = 5 ,message = "Department extension no. cannot be greater than 5")
+    @Column (name = "extension_no" ,unique = false ,nullable = true)
     private Long extensionNo;
 
     public Department() {
