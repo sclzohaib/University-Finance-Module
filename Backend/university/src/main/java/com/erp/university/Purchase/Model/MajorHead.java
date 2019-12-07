9fbae507e83c98fc46a531928f23c546f448d14e
dell <sunduskanwal4>
2019-12-07 05:11:22 -0800

    Major Head JSR Validation work done ..sundus

--- Backend/university/src/main/java/com/erp/university/Purchase/Model/MajorHead.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/Model/MajorHead.java
@@ -1,13 +1,28 @@
 package com.erp.university.Purchase.Model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
+@Table (name = "major_head")
 public class MajorHead {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_sequence")
     @SequenceGenerator(name = "id_sequence", sequenceName = "major_head_seq")
+
+    @NotNull(message = "Major head ID cannot be null")
+    @NotEmpty(message = "Major head ID cannot be empty")
+    @Column(name = "id" ,unique = true,nullable = false)
     private Long id;
+
+    @NotNull(message = "Name cannot be null")
+    @NotEmpty(message = "Name cannot be empty")
+    @NotBlank(message = "Name cannot be blank")
+    @Column(name = "name",unique = false,nullable = false)
+    @Size(min = 4 ,max = 50 ,message = "Major head name must be in between 11 and 13")
     private String name;
 
     public MajorHead() {
