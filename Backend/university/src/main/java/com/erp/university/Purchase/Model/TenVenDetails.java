b22c39d055ef28764c56d815c67bdc77fa714449
Muhammad mohsin <muhammadmohsinkhan864@gmail.com>
2019-12-08 00:45:14 +0500

    TenVenDeatls-jsr-validation (sadia)

--- Backend/university/src/main/java/com/erp/university/Purchase/Model/TenVenDetails.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/Model/TenVenDetails.java
@@ -2,18 +2,50 @@ package com.erp.university.Purchase.Model;
 
 
 import javax.persistence.*;
+import javax.validation.constraints.*;
 
 @Entity
+@Table(name = "tenvendetails")
 public class TenVenDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_sequence")
     @SequenceGenerator(name = "id_sequence", sequenceName = "ten_ven_details_seq")
+    @NotNull(message = "ID cannot be null")
+    @NotEmpty(message = "ID cannot be empty")
+    @Column(name = "id", unique = true, nullable = false)
     private Long id;
+
+    @NotNull(message = "Quotation cannot be null !!")
+    @NotBlank(message = "Quotation cannot be blank !!")
+    @NotEmpty(message = "Quotation cannot be empty")
+    @Column(name = "quotation", unique = true, nullable = false)
     private String quotation;
+
+    @NotNull(message = "Email address cannot be null !!")
+    @NotBlank(message = "Email address cannot be blank !!")
+    @NotEmpty(message = "Email address cannot be empty")
+    @Email(message ="Email should be valid")
+    @Column(name = "vendor_email", unique = true, nullable = false)
     private String vendorEmail;
+
+    @NotNull(message = "Vendor Address cannot be null !!")
+    @NotBlank(message = "Vendor Address cannot be blank !!")
+    @NotEmpty(message = "Vendor Address cannot be empty")
+    @Column(name = "vendor_address" ,unique = true, nullable = false)
     private String vendorAddress;
+
+    @NotNull(message = "VenNtn cannot be null !!")
+    @NotBlank(message = "VenNtn cannot be blank !!")
+    @NotEmpty(message = "VenNtn cannot be empty")
+    @Column(name = "ven_ntn" ,unique = true, nullable = false)
     private String venNtn;
+
+    @NotNull(message = "Vendor NIC cannot be null !!")
+    @NotBlank(message = "Vendor NIC cannot be blank !!")
+    @NotEmpty(message = "Vendor NIC cannot be empty")
+    @Column(name = "vendor_nic" ,unique = true, nullable = false)
     private Long vendorNic;
+
 
     public TenVenDetails() {
     }
