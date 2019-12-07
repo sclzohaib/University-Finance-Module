0f6d92ecb9aa5f675f0cd11aeb767fa468bfc414
Muhammad mohsin <muhammadmohsinkhan864@gmail.com>
2019-12-08 01:20:00 +0500

    Quotation Detail-jsr-validation (sadia)

--- Backend/university/src/main/java/com/erp/university/Purchase/Controller/QuotationDetailController.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/Controller/QuotationDetailController.java
@@ -9,6 +9,8 @@ import org.springframework.data.querydsl.QuerydslUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
 import java.util.List;
 
 @RestController
@@ -20,7 +22,7 @@ public class QuotationDetailController {
 
     //Post Call
     @RequestMapping (value ="/",method =RequestMethod.POST)
-    public ResponseEntity<String> saveQuotationDetail(@RequestBody QuotationDetailDTO quotationDetailDTO){
+    public ResponseEntity<String> saveQuotationDetail(@Valid @RequestBody QuotationDetailDTO quotationDetailDTO){
        return quotationDetailService.saveQuotationDetail(quotationDetailDTO);
     }
 
@@ -32,13 +34,13 @@ public class QuotationDetailController {
 
     //Get By id
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<QuotationDetail> getQuotationDetailById(@PathVariable("id") Long id){
+    public ResponseEntity<QuotationDetail> getQuotationDetailById(@PathVariable("id") @Min(value = 1, message = "ID must be greater than 1")Long id){
         return quotationDetailService.getQuotationDetailById(id);
     }
 
     //Update Quotation Detail
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<String> updateQuotationDetail(@PathVariable("id") Long id, @RequestBody QuotationDetailDTO quotationDetailDTO){
+    public ResponseEntity<String> updateQuotationDetail(@PathVariable("id") @Min(value = 1, message = "ID must be greater than 1") Long id, @Valid @RequestBody QuotationDetailDTO quotationDetailDTO){
         return quotationDetailService.updateQuotationDetail(id, quotationDetailDTO);
     }
 
--- Backend/university/src/main/java/com/erp/university/Purchase/Model/QuotationDetail.java
+++ Backend/university/src/main/java/com/erp/university/Purchase/Model/QuotationDetail.java
@@ -1,18 +1,55 @@
 package com.erp.university.Purchase.Model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
+@Table(name = "quotation_detail")
 public class QuotationDetail {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "id_sequence")
     @SequenceGenerator(name = "id_sequence", sequenceName = "qd_sequence")
+    @NotNull(message = "ID cannot be null")
+    @NotEmpty(message = "ID cannot be empty")
+    @Column(name = "id", unique = true, nullable = false)
     private Long id;
+
+    @NotNull(message = "Description cannot be null !!")
+    @NotBlank(message = "Description cannot be blank !!")
+    @NotEmpty(message = "Description cannot be empty")
+    @Column(name = "description", unique = true, nullable = false)
     private String description;
+
+    @NotNull(message = "Brand cannot be null !!")
+    @NotBlank(message = "Brand cannot be blank !!")
+    @NotEmpty(message = "Brand cannot be empty")
+    @Column(name = "brand", nullable = false)
     private String Brand;
+
+    @NotNull(message = "Quantity cannot be null !!")
+    @NotBlank(message = "Quantity  cannot be blank !!")
+    @NotEmpty(message = "Quantity cannot be empty")
+    @Column(name = "quantity", unique = true, nullable = false)
     private Long quantity;
+
+    @NotNull(message = "Price with GST cannot be null !!")
+    @NotBlank(message = "Price with GST cannot be blank !!")
+    @NotEmpty(message = "Price with GST cannot be empty")
+    @Column(name = "price_with_gst", unique = true, nullable = false)
     private Long priceWithGst;
+
+    @NotNull(message = "Total cannot be null !!")
+    @NotBlank(message = "Total cannot be blank !!")
+    @NotEmpty(message = "Total cannot be empty")
+    @Column(name = "total", unique = true, nullable = false)
     private Long total;
+
+    @NotNull(message = "Unit Price cannot be null !!")
+    @NotBlank(message = "Unit Price cannot be blank !!")
+    @NotEmpty(message = "Unit Price  cannot be empty")
+    @Column(name = "unit_price", unique = true, nullable = false)
     private Long unitPrice;
 
     public QuotationDetail() {
